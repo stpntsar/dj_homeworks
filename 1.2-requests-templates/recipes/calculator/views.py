fc98598d1ac0b1f0d7f8527990407ce6294c3aae
Степан Царегородцев <stpncar@gmail.com>
2022-11-22 21:17:51 +0300

    recipes

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,5 @@
+from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +18,35 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def omlet(request):
+    servings = int(request.GET.get('servings'))
+    context = {
+        'recipe': {
+        'яйца, шт': 2 * servings,
+        'молоко, л': 0.1 * servings,
+        'соль, ч.л.': 0.5 * servings,
+        },
+    }
+    return render(request, 'calculator/index.html', context)
+def pasta(request):
+    servings = int(request.GET.get('servings'))
+    context = {
+        'recipe': {
+        'макароны, г': 0.3 * servings,
+        'сыр, г': 0.05 * servings,
+        },
+    }
+    return render(request, 'calculator/index.html', context)
+def buter(request):
+    servings = int(request.GET.get('servings'))
+    context = {
+        'recipe': {
+        'хлеб, ломтик': 1 * servings,
+        'колбаса, ломтик': 1 * servings,
+        'сыр, ломтик': 1 * servings,
+        'помидор, ломтик': 1 * servings,
+        },
+    }
+    return render(request, 'calculator/index.html', context)
