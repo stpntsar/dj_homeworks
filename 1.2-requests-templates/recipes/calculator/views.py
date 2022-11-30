415e475a5b89407c418a3ceb88426ebea1a16ca5
Степан Царегородцев <stpncar@gmail.com>
2022-11-30 21:11:39 +0300

    recipes

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -20,33 +20,14 @@ DATA = {
     },
 }
 
-def omlet(request):
-    servings = int(request.GET.get('servings'))
-    context = {
-        'recipe': {
-        'яйца, шт': 2 * servings,
-        'молоко, л': 0.1 * servings,
-        'соль, ч.л.': 0.5 * servings,
-        },
-    }
-    return render(request, 'calculator/index.html', context)
-def pasta(request):
-    servings = int(request.GET.get('servings'))
-    context = {
-        'recipe': {
-        'макароны, г': 0.3 * servings,
-        'сыр, г': 0.05 * servings,
-        },
-    }
-    return render(request, 'calculator/index.html', context)
-def buter(request):
-    servings = int(request.GET.get('servings'))
-    context = {
-        'recipe': {
-        'хлеб, ломтик': 1 * servings,
-        'колбаса, ломтик': 1 * servings,
-        'сыр, ломтик': 1 * servings,
-        'помидор, ломтик': 1 * servings,
-        },
-    }
-    return render(request, 'calculator/index.html', context)
+def dish_view(request, dish):
+
+    if dish in DATA.keys():
+        servings = int(request.GET.get('servings'))
+        ingr = {}
+        for ingridient in DATA[dish]:
+            ingr[ingridient] = DATA[dish][ingridient] * servings
+            context = {
+                    'recipe': ingr
+                }
+        return render(request, 'calculator/index.html', context)
